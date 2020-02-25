51402589cdaca5eed8adfc51b539c88c0d87757f
SergiyDanchuk <sergey.danchuk.p@gmail.com>
2020-02-25 21:27:11 +0200

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,18 @@
 $(function () { 
 
-
+    var mySwiper = new Swiper ('#hotel-room__slider' , {
+        // Optional parameters
+        loop: true,
+        spaceBetween: 100,
+        // If we need pagination
+        pagination: {
+          el: '.swiper-pagination',
+        },
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      })
     
 });
